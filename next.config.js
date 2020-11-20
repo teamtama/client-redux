b36cc8cee679fc480fde983e9a4984f93e6c23a3
jonsoku2 <whdtjr2792@gmail.com>
2020-11-21 01:43:44 +0900

    init project

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  publicRuntimeConfig: {
+    TEST: process.env.TEST,
+  },
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react'
+import { TEST } from '../../config'
+
+interface Props {}
+
+const Index: FunctionComponent<Props> = () => {
+  return <div>{TEST}</div>
+}
+
+export default Index
